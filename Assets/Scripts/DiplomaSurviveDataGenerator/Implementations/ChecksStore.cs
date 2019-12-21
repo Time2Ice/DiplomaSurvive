2ad4077df61311b50948ce2534c6b3517ac0fa72
Milaila <milailatrens@gmail.com>
2019-12-21 09:01:48 +0200

    Adds ChecksStore class

---
+++ Assets/Scripts/DiplomaSurviveDataGenerator/Implementations/ChecksStore.cs
@@ -0,0 +1,33 @@
+﻿using DiplomaSurviveDataGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.DiplomaSurviveDataGenerator.Implementations
+{
+    public class ChecksStore : BaseStore<BaseCheck>
+    {
+        public ChecksStore(INumberGenerator generator, IPlayContext context)
+            : base(GetChecks(context), generator)
+        {
+        }
+
+        public static List<BaseCheck> GetChecks(IPlayContext context)
+        {
+            return new List<BaseCheck>()
+            {
+                new BaseCheck(ExclusionType.PersonalLife, new List<ICheckStep>
+                {
+                    new StudyScoreExcessPercentCheckStep(context) { Percent = 0.5},
+                    new PersonalLifeScoreShortagePercentCheckStep(context) { Percent = 0.5, ExclusionProbability = 0.001 },
+                }),
+                new BaseCheck(ExclusionType.PersonalLife, new List<ICheckStep>
+                {
+                    new PersonalLifeScoreShortageCheckStep(context) { MinScore = 0, ExclusionProbability = 1 },
+                }) { Priority = 1 },
+            };
+        }
+    }
+}
