970d1aa9080807d5fa8f034cb1340a33a5163cf7
Milaila <milailatrens@gmail.com>
2019-12-21 09:04:36 +0200

    Implements BaseStudy context

--- Assets/Scripts/DiplomaSurviveDataGenerator/Library/Contexts/BaseStudyContext.cs
+++ Assets/Scripts/DiplomaSurviveDataGenerator/Library/Contexts/BaseStudyContext.cs
@@ -8,5 +8,37 @@ namespace DiplomaSurviveDataGenerator
 {
     public class BaseStudyContext : Context, IStudyContext
     {
+        protected bool _hasBook = false;
+        protected ExclusionType _needExclusion = ExclusionType.Undefined;
+        public virtual bool HasBook
+        {
+            get { return _hasBook; }
+            set
+            {
+                _hasBook = value;
+                OnHasBookChanged?.Invoke();
+            }
+        }
+        public virtual ExclusionType NeedExclusion
+        {
+            get
+            {
+                return _needExclusion;
+            }
+            set
+            {
+                _needExclusion = value;
+                OnNeedExclusionChanged?.Invoke();
+            }
+        }
+
+        public virtual event ValueChanged OnHasBookChanged;
+        public virtual event ValueChanged OnNeedExclusionChanged;
+
+        public BaseStudyContext()
+        {
+            OnHasBookChanged += ContextChanged;
+            OnNeedExclusionChanged += ContextChanged;
+        }
     }
 }
